74f2d1d97db1b123a17f417e57815cc9c1e4f1c4
amarosuarez <amaro.suarez@iesnervion.es>
2024-12-03 14:10:45 +0100

    api que devuelve todos hecho

---
+++ ASP/Controllers/API/PersonasController.cs
@@ -0,0 +1,45 @@
+﻿using BL;
+using ENT;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASP.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        // GET: api/<PersonasController>
+        [HttpGet]
+        public List<clsPersona> Get()
+        {
+            return clsListadosPersonasBL.listadoCompletoPersonasBL();
+        }
+
+        // GET api/<PersonasController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonasController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
